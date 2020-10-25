9e5c770ed7f75b8e9f5084395a03126488f62b3d
Wally Yuen <wally.yuen@ogd.nl>
2020-10-25 08:38:22 +0100

    Assign role for users when logging in

--- models/userModel.js
+++ models/userModel.js
@@ -7,7 +7,15 @@ const UserModel = types
     email: types.maybeNull(types.string),
     isOnline: types.optional(types.boolean, false),
     photoURL: types.maybeNull(types.string),
+    role: types.optional(
+      types.enumeration("Role", ["student", "instructor", "actor"]), "student",
+    ),
   })
+  .actions(self => ({
+    setRole(isAdmin) {
+      self.role = isAdmin ? "instructor" : "student";
+    },
+  }))
   .views(self => ({
     get name() {
       return self.displayName ?? `Anonymous (${self.email})`;
