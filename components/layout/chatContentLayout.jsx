f871d22ba4e3086e783b6b3760d599ff3ce40a52
Wally Yuen <wally.yuen@ogd.nl>
2020-10-28 08:52:16 +0100

    Fix typo

--- components/layout/chatContentLayout.jsx
+++ components/layout/chatContentLayout.jsx
@@ -18,24 +18,22 @@ const ChatContentLayout = ({
   enableStaticRendering(typeof window === "undefined");
 
   return (
-    <div>
-      <div className="chat-area" ref={parentRef}>
-        {isLoading && (
-          <div className="spinner-border text-success" role="status">
-            <span className="sr-only">Loading...</span>
-          </div>
-        )}
-        {readError && <p className="text-danger">{readError}</p>}
-        {!isLoading && !readError && chatHistory.map(message => (
-          <p key={message.timestamp} className={clsx("chat-bubble", { "current-user": user.uid === message.user?.uid })}>
-            {message.content}
-            <br />
-            <span className="chat-time float-right">{formatTime(message.timestamp)}</span>
-            <br />
-            <span>{message.createdBy}</span>
-          </p>
-        ))}
-      </div>
+    <div className="chat-area" ref={parentRef}>
+      {isLoading && (
+        <div className="spinner-border text-success" role="status">
+          <span className="sr-only">Loading...</span>
+        </div>
+      )}
+      {readError && <p className="text-danger">{readError}</p>}
+      {!isLoading && !readError && chatHistory.map(message => (
+        <p key={message.timestamp} className={clsx("chat-bubble", { "current-user": user.uid === message.user?.uid })}>
+          {message.content}
+          <br />
+          <span className="chat-time float-right">{formatTime(message.timestamp)}</span>
+          <br />
+          <span>{message.createdBy}</span>
+        </p>
+      ))}
     </div>
   );
 };
