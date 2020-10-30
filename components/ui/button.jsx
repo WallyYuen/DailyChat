c31cfc15e53473994e422ef06d861e6140cbf466
Wally Yuen <wally.yuen@ogd.nl>
2020-10-30 23:14:21 +0100

    Remove unused classes

--- components/ui/button.jsx
+++ components/ui/button.jsx
@@ -2,7 +2,7 @@ import React, { forwardRef } from "react";
 
 const Button = forwardRef(({ onClick, label, ...props }, ref) => {
   const classes = props.classes || [];
-  const defaultClasses = ["btn", "px-5", ...classes];
+  const defaultClasses = [...classes];
 
   return (
     <button className={defaultClasses.join(" ")} onClick={onClick} {...props} ref={ref}>
