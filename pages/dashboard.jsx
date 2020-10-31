2acfcb9260e6ea952944a362dd80ff2e81aaac52
Wally Yuen <wally.yuen@ogd.nl>
2020-10-31 01:32:47 +0100

    Add listener for catching esc key to hide modal

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { hideAll } from "tippy.js";
 import { PrivateRoute } from "lib/routing";
 
 // Components
 import DashboardLayout from "components/layout/dashboardLayout";
 
 const Dashboard = () => {
+  const handleEsc = (event) => {
+    if (event.keyCode !== 27) return;
+
+    hideAll();
+  };
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleEsc);
+
+    return () => {
+      window.removeEventListener("keydown", handleEsc);
+    };
+  }, []);
+
   return (
     <PrivateRoute>
       <DashboardLayout />
