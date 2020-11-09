175bbb339d9640c2089d17b6ce17e5b267240b8a
Wally Yuen <wally.yuen@ogd.nl>
2020-11-09 05:41:54 +0100

    Add SVG icons

---
+++ components/icon/dropdownIcon.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const DropdownIcon = (props) => {
+  return(
+    <svg {...props} xmlns="http://www.w3.org/2000/svg" width="7" height="12" viewBox="0 0 7 12">
+      <g transform="translate(-4758.87 -6493.5)">
+        <path d="M3,0,6,3ZM3,0,0,3Z" transform="translate(4759.37 6494)" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1"/>
+        <path d="M3,3,6,0ZM3,3,0,0Z" transform="translate(4759.37 6502)" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1"/>
+      </g>
+    </svg>
+  );
+};
+
+export default DropdownIcon;
+
+
