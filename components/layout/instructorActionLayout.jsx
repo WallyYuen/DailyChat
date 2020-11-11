b035c8e38729e905c74128c0ae31f74fdd6dfcf4
Wally Yuen <wally.yuen@ogd.nl>
2020-11-11 03:57:57 +0100

    Add callback for handling after modal is closed

--- components/layout/instructorActionLayout.jsx
+++ components/layout/instructorActionLayout.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 // Component
 import CatalogEditor from "components/container/catalogEditor";
+import CatalogSettings from "components/container/catalogSettings";
 
 // UI
+import Modal from "components/ui/modal";
 import Button from "components/ui/button";
 
 // Styling
@@ -11,7 +13,10 @@ import layout from "components/layout/instructorActionLayout.module.scss";
 import button from "components/ui/button.module.scss";
 
 const InstructorActionLayout = ({ catalogProps }) => {
+  const [callback, setCallback] = useState(() => () => {});
   const { openCatalog, catalogIsOpen } = catalogProps;
+
+  const onCloseModal = () => callback();
 
   return (
     <div className={layout.container}>
@@ -19,7 +24,13 @@ const InstructorActionLayout = ({ catalogProps }) => {
         <span>Instructor actions</span>
       </div>
       <div className={layout.buttonContainer}>
-        <Button className={button.neutral} label="Catalog" onClick={openCatalog} />
+        <Button className={button.neutral} label="catalog editor" onClick={openCatalog} />
+        <Modal
+          content={<CatalogSettings modalCallback={setCallback} />}
+          callback={onCloseModal}
+        >
+          <Button className={button.neutral} label="catalog settings" />
+        </Modal>
       </div>
       {catalogIsOpen && <CatalogEditor />}
     </div>
